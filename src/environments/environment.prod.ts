2d6aaceacda4c9f1dc39a70f0c9c65924e084959
joseadro09 <jadrianparedes10@gmail.com>
2026-09-14 14:24:32 -0500

    Update production API base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://backpulsereport-production-7576.up.railway.app/api/v1',
+  apiBaseUrl: 'https://backend-nursepulse.onrender.com/api/v1',
   appName: 'Care-Labs / NursePulse',
 };
